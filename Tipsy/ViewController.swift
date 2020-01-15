8153efd99227364dbb7551a956e2fef9ccc43f10
Prahlad Anand <anprahlad@gmail.com>
2020-01-15 18:56:41 -0500

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Prahlad Anand on 1/15/20.
+//  Copyright © 2020 Prahlad Anand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
